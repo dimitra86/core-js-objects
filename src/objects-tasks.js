10bfaab403ad7c0945ff2d167ddcbb961c497225
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-03-07 10:46:37 +0300

    feat: finish fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -179,8 +179,12 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  // throw new Error('Not implemented');
+  const obj = JSON.parse(json);
+  const instance = Object.create(proto);
+  Object.assign(instance, obj);
+  return instance;
 }
 
 /**
